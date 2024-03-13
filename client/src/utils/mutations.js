64bd2dad9884501817d712c31ee9d1d6c9de7411
Matt R <mattrallo2012@gmail.com>
2024-03-13 00:20:28 -0500

    made util files for front end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,64 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER  = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER  = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK  = gql`
+    mutation saveBook($bookId: String!, $description: String!, $title: String! $authors: [String], $image: String, $link: String) {
+        saveBook(bookId: $bookId, description: $description, title: $title authors: $authors, image: $image, link: $link) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const DELETE_BOOK  = gql`
+    mutation deleteBook($bookId: String!) {
+        deleteBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
+        }
+    }
+`;
